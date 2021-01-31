49b2f85c41ee881ef66264e40d8c6288cdcefaec
Egor Halimonenko <termi_uc@inbox.ru>
2021-01-31 20:03:38 +0300

    feat: initial commit *2 runEnv.ts

--- runEnv.ts
+++ runEnv.ts
@@ -1,16 +1,23 @@
 'use strict';
 
-let isNodeJS = false;
+// https://stackoverflow.com/a/23619712
+// https://github.com/emscripten-core/emscripten/blob/54b0f19d9e8130de16053b0915d114c346c99f17/src/shell.js
+// var ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function';
+// var ENVIRONMENT_IS_WEB = typeof window === 'object';
+// var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
+// var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
 
-if (typeof process !== 'undefined') {
-    isNodeJS = process && typeof process === 'object';// Maybe this is Node.js
+let ENVIRONMENT_IS_NODE = false;
 
-    if (isNodeJS) {
+if (typeof process !== 'undefined' && typeof require === 'function') {
+    ENVIRONMENT_IS_NODE = process && typeof process === 'object';// Maybe this is Node.js
+
+    if (ENVIRONMENT_IS_NODE) {
         if (typeof window !== 'undefined') {
             if (window["__fake__"]) {
                 // (jsdom is used automatically)[https://github.com/facebook/jest/issues/3692#issuecomment-304945928]
                 // workaround for jest+JSDOM
-                isNodeJS = true;
+                ENVIRONMENT_IS_NODE = true;
             }
             else if (window["jasmine"]) {
                 // jest/jasmine tests environment
@@ -27,16 +34,16 @@ if (typeof process !== 'undefined') {
                 }
 
                 if (isJasmine) {
-                    isNodeJS = true;
+                    ENVIRONMENT_IS_NODE = true;
                 }
             }
             else {
-                isNodeJS = false;
+                ENVIRONMENT_IS_NODE = false;
             }
         }
         else if (process["browser"]) {
             // babel process shim
-            isNodeJS = false;
+            ENVIRONMENT_IS_NODE = false;
         }
         // else {
         //     isNodeJS === true
@@ -44,5 +51,8 @@ if (typeof process !== 'undefined') {
     }
 }
 
-/** @type {boolean} */
-module.exports = /** @type {boolean} */isNodeJS;
+const ENVIRONMENT_IS_WEB_WORKER = !ENVIRONMENT_IS_NODE && typeof importScripts === 'function' && typeof document === 'undefined';
+
+export const isNodeJS = ENVIRONMENT_IS_NODE;
+export const isWeb = !ENVIRONMENT_IS_NODE;
+export const isWebWorker = ENVIRONMENT_IS_WEB_WORKER;
